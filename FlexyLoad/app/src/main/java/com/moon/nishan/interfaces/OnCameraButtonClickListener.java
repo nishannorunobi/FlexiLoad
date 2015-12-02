4a75ce46c02020c6a681b00b1af2dbeb84600250
nishan <nishancse@gmail.com>
2015-12-03 00:09:44 +0600

    Previous project deleted

--- FlexyLoad/app/src/main/java/com/moon/nishan/interfaces/OnCameraButtonClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.moon.nishan.interfaces;
-
-/**
- * Created by nishan on 11/28/15.
- */
-public interface OnCameraButtonClickListener {
-    void turnOn();
-    void turnOff();
-}
--- FlexyLoad/app/src/main/java/com/moon/nishan/interfaces/OnFlashButtonClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.moon.nishan.interfaces;
-
-/**
- * Created by nishan on 12/1/15.
- */
-public interface OnFlashButtonClickListener {
-    void turnOn();
-    void turnOff();
-}
